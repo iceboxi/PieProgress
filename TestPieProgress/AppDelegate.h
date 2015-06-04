431381ce81e085cb1e9fee27fcdace26cefa796e
iceboxi <iceboxi@gmail.com>
2015-06-04 11:58:14 +0800

    Initial Commit

---
+++ TestPieProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPieProgress
+//
+//  Created by iceboxi on 2015/6/4.
+//  Copyright (c) 2015年 iceboxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
